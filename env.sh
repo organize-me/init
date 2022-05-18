f9239bed36713ec6745e91d34a60eda68448dff7
isaiah-v <isaiah.v@comcast.net>
2022-05-17 23:20:12 -0700

    Added env.sh

---
+++ env.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 1. Set applicable fields (TZ, DOMAIN, etc...)
+# 2. Move to ~/bin/env.sh
+# 3. Append .profile to include this script '. bin/env.sh'
+
+
+# --== Timezone ==- #
+export TZ=America/Los_Angeles
+
+# --== Domain ==-- #
+export DOMAIN=vanderelst.house
+export PUBLIC_IP=$(wget -qO- https://ipecho.net/plain)
+
+# --== Terraform ==-- #
+export TF_VAR_domain=$DOMAIN
+export TF_VAR_public_ip=$PUBLIC_IP
+export TF_VAR_timezone=$TZ
